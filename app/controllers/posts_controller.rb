b81da75216bc1ae12f6ee659742929a58e44629f
Dan Winslow <Dwinslow123@me.com>
2018-11-25 15:03:59 -0600

    working CRUD app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:edit, :update, :show, :delete]
+  before_action :find_post, only: [:edit, :update, :show, :destroy]
 
   # Index action to render all posts
   def index
@@ -11,41 +11,46 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # Create action to save the post into the database
+  # Create action saves the post into database
   def create
-    @post = Post.new
-    if @post.save(post_params)
-      flash[:notice] = "Succesfully created post!"
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Successfully created post!"
       redirect_to post_path(@post)
     else
-      flash[:alert] = "Error creating post!"
+      flash[:alert] = "Error creating new post!"
       render :new
     end
   end
 
-  # Edit action retrieves the post and renders the edit page
+  # Edit action retrives the post and renders the edit page
   def edit
+    @post = Post.find(params[:id])
   end
 
-  # Update action updates the post with new information
+  # Update action updates the post with the new information
   def update
-    if @post.update_attributes(post_params)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
       flash[:notice] = "Successfully updated post!"
+      redirect_to post_path(@post)
     else
       flash[:alert] = "Error updating post!"
+      render :edit
     end
   end
 
-  # The show action renders the individual post after retrieving the ID
+  # The show action renders the individual post after retrieving the the id
   def show
   end
 
   # The destroy action removes the post permanently from the database
   def destroy
-    if @post.destroy
-      flash[:notice] = "Successfully destroyed post!"
+    if @post.delete
+      flash[:notice] = "Successfully deleted post!"
+      redirect_to posts_path
     else
-      flash[:alert] = "Error deleting post!"
+      flash[:alert] = "Error updating post!"
     end
   end
 
@@ -58,5 +63,4 @@ class PostsController < ApplicationController
   def find_post
     @post = Post.find(params[:id])
   end
-  
 end
